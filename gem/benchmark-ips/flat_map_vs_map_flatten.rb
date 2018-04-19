663198876e5a31640aba1810326e93d277a09e98
kurikei <kurikei@gmail.com>
2018-04-19 18:20:30 +0900

    add benchmark sandbox code

---
+++ gem/benchmark-ips/flat_map_vs_map_flatten.rb
@@ -0,0 +1,27 @@
+# http://work.stevegrossi.com/2014/10/25/writing-fast-and-idiomatic-ruby/
+require 'benchmark/ips'
+# ----- conclusion -----
+# flat_map は map.flatten より約2倍早い
+#
+# ----- benchmark result -----
+# Warming up --------------------------------------
+#             flat_map    26.387k i/100ms
+#          map.flatten    12.537k i/100ms
+# Calculating -------------------------------------
+#             flat_map    287.810k (± 1.3%) i/s -      1.451M in   5.054310s
+#          map.flatten    131.341k (± 1.1%) i/s -    664.461k in   5.068302s
+#                    with 95.0% confidence
+#
+# Comparison:
+#             flat_map:   287809.6 i/s
+#          map.flatten:   131341.0 i/s - 2.19x  (± 0.04) slower
+#                    with 95.0% confidence
+
+Benchmark.ips do |x|
+  x.config(stats: :bootstrap, confidence: 95)
+
+  x.report('flat_map')    { 20.times.flat_map { |i| [i] } }
+  x.report('map.flatten') { 20.times.map { |i| [i] }.flatten }
+
+  x.compare!
+end
---
+++ gem/benchmark-ips/sub_vs_gsub.rb
@@ -0,0 +1,27 @@
+require 'benchmark/ips'
+# ----- conclusion -----
+# sub は gsub より3倍以上早いので不要なら不用意に用いない
+#
+# ----- benchmark result -----
+# Warming up --------------------------------------
+#                  sub   121.639k i/100ms
+#                 gsub    39.958k i/100ms
+# Calculating -------------------------------------
+#                  sub      1.805M (± 1.5%) i/s -      9.001M in   5.007459s
+#                 gsub    496.100k (± 1.6%) i/s -      2.477M in   5.013399s
+#                    with 95.0% confidence
+#
+# Comparison:
+#                  sub:  1805241.3 i/s
+#                 gsub:   496099.9 i/s - 3.64x  (± 0.08) slower
+#                    with 95.0% confidence
+
+string = 'google_cloud_platform'
+Benchmark.ips do |x|
+  x.config(stats: :bootstrap, confidence: 95)
+
+  x.report('sub')    { string.sub(/\Agoogle_/, '') }
+  x.report('gsub')   { string.gsub(/\Agoogle_/, '') }
+
+  x.compare!
+end
